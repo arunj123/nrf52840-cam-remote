ea18941c930c41bb7767a43ac0a4c9f14f04158e
Arun J <arunj123@gmail.com>
2026-02-28 15:44:44 +0100

    Initial commit for nRF52840 BLE HID Camera Remote

---
+++ app/src/hog.c
@@ -0,0 +1,197 @@
+/** @file
+ *  @brief HoG Service sample
+ */
+
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <zephyr/drivers/gpio.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+
+enum {
+	HIDS_REMOTE_WAKE = BIT(0),
+	HIDS_NORMALLY_CONNECTABLE = BIT(1),
+};
+
+struct hids_info {
+	uint16_t version; /* version number of base USB HID Specification */
+	uint8_t code; /* country HID Device hardware is localized for. */
+	uint8_t flags;
+} __packed;
+
+struct hids_report {
+	uint8_t id; /* report id */
+	uint8_t type; /* report type */
+} __packed;
+
+static struct hids_info info = {
+	.version = 0x0000,
+	.code = 0x00,
+	.flags = HIDS_NORMALLY_CONNECTABLE,
+};
+
+enum {
+	HIDS_INPUT = 0x01,
+	HIDS_OUTPUT = 0x02,
+	HIDS_FEATURE = 0x03,
+};
+
+static struct hids_report input = {
+	.id = 0x01,
+	.type = HIDS_INPUT,
+};
+
+static uint8_t simulate_input;
+static uint8_t ctrl_point;
+static uint8_t report_map[] = {
+	0x05, 0x0C, /* Usage Page (Consumer) */
+	0x09, 0x01, /* Usage (Consumer Control) */
+	0xA1, 0x01, /* Collection (Application) */
+	0x85, 0x01, /*   Report ID (1) */
+	0x15, 0x00, /*   Logical Minimum (0) */
+	0x25, 0x01, /*   Logical Maximum (1) */
+	0x75, 0x01, /*   Report Size (1) */
+	0x95, 0x01, /*   Report Count (1) */
+	0x09, 0xE9, /*   Usage (Volume Increment) */
+	0x81, 0x02, /*   Input (Data,Var,Abs,No Wrap,Linear,...) */
+	0x95, 0x07, /*   Report Count (7) for padding */
+	0x81, 0x03, /*   Input (Const,Var,Abs,No Wrap,Linear,...) */
+	0xC0,       /* End Collection */
+};
+
+
+static ssize_t read_info(struct bt_conn *conn,
+			  const struct bt_gatt_attr *attr, void *buf,
+			  uint16_t len, uint16_t offset)
+{
+	return bt_gatt_attr_read(conn, attr, buf, len, offset, attr->user_data,
+				 sizeof(struct hids_info));
+}
+
+static ssize_t read_report_map(struct bt_conn *conn,
+			       const struct bt_gatt_attr *attr, void *buf,
+			       uint16_t len, uint16_t offset)
+{
+	return bt_gatt_attr_read(conn, attr, buf, len, offset, report_map,
+				 sizeof(report_map));
+}
+
+static ssize_t read_report(struct bt_conn *conn,
+			   const struct bt_gatt_attr *attr, void *buf,
+			   uint16_t len, uint16_t offset)
+{
+	return bt_gatt_attr_read(conn, attr, buf, len, offset, attr->user_data,
+				 sizeof(struct hids_report));
+}
+
+static void input_ccc_changed(const struct bt_gatt_attr *attr, uint16_t value)
+{
+	simulate_input = (value == BT_GATT_CCC_NOTIFY) ? 1 : 0;
+}
+
+static ssize_t read_input_report(struct bt_conn *conn,
+				 const struct bt_gatt_attr *attr, void *buf,
+				 uint16_t len, uint16_t offset)
+{
+	return bt_gatt_attr_read(conn, attr, buf, len, offset, NULL, 0);
+}
+
+static ssize_t write_ctrl_point(struct bt_conn *conn,
+				const struct bt_gatt_attr *attr,
+				const void *buf, uint16_t len, uint16_t offset,
+				uint8_t flags)
+{
+	uint8_t *value = attr->user_data;
+
+	if (offset + len > sizeof(ctrl_point)) {
+		return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
+	}
+
+	memcpy(value + offset, buf, len);
+
+	return len;
+}
+
+#if CONFIG_SAMPLE_BT_USE_AUTHENTICATION
+/* Require encryption using authenticated link-key. */
+#define SAMPLE_BT_PERM_READ BT_GATT_PERM_READ_AUTHEN
+#define SAMPLE_BT_PERM_WRITE BT_GATT_PERM_WRITE_AUTHEN
+#else
+/* Require encryption. */
+#define SAMPLE_BT_PERM_READ BT_GATT_PERM_READ_ENCRYPT
+#define SAMPLE_BT_PERM_WRITE BT_GATT_PERM_WRITE_ENCRYPT
+#endif
+
+/* HID Service Declaration */
+BT_GATT_SERVICE_DEFINE(hog_svc,
+	BT_GATT_PRIMARY_SERVICE(BT_UUID_HIDS),
+	BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_INFO, BT_GATT_CHRC_READ,
+			       BT_GATT_PERM_READ, read_info, NULL, &info),
+	BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_REPORT_MAP, BT_GATT_CHRC_READ,
+			       BT_GATT_PERM_READ, read_report_map, NULL, NULL),
+	BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_REPORT,
+			       BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY,
+			       SAMPLE_BT_PERM_READ,
+			       read_input_report, NULL, NULL),
+	BT_GATT_CCC(input_ccc_changed,
+		    SAMPLE_BT_PERM_READ | SAMPLE_BT_PERM_WRITE),
+	BT_GATT_DESCRIPTOR(BT_UUID_HIDS_REPORT_REF, BT_GATT_PERM_READ,
+			   read_report, NULL, &input),
+	BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_CTRL_POINT,
+			       BT_GATT_CHRC_WRITE_WITHOUT_RESP,
+			       BT_GATT_PERM_WRITE,
+			       NULL, write_ctrl_point, &ctrl_point),
+);
+
+void hog_init(void)
+{
+}
+
+#define SW0_NODE DT_ALIAS(sw0)
+static const struct gpio_dt_spec sw0 = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+static struct gpio_callback button_cb_data;
+
+static void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+	if (!simulate_input) {
+		return;
+	}
+
+	uint8_t report = 0;
+	if (gpio_pin_get_dt(&sw0)) {
+		report |= BIT(0); /* Volume Increment pressed */
+		printk("Button pressed, sending Vol UP (Camera Shutter)\n");
+	} else {
+		printk("Button released, sending empty report\n");
+	}
+
+	bt_gatt_notify(NULL, &hog_svc.attrs[5], &report, sizeof(report));
+}
+
+void hog_button_loop(void)
+{
+	gpio_pin_configure_dt(&sw0, GPIO_INPUT);
+	gpio_pin_interrupt_configure_dt(&sw0, GPIO_INT_EDGE_BOTH);
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(sw0.pin));
+	gpio_add_callback(sw0.port, &button_cb_data);
+
+	printk("HID Ready out of loop. Deep sleeping...\n");
+	k_sleep(K_FOREVER);
+}
